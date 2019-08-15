f452e3e578c5a79d8e26910d9d0c096ca0fb425b
jychuah <jychuah@gmail.com>
2019-08-15 14:57:56 -0500

    Alternate way of getting IP

--- server/index.js
+++ server/index.js
@@ -11,6 +11,35 @@ const os = require('os');
 const ip = require('ip');
 const { exec } = require('child_process');
 
+var ifaces = os.networkInterfaces();
+var ipaddress = null;
+
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ipaddress = iface.address;
+    ++alias;
+  });
+});
+
+if (!ipaddress) {
+  ipaddress = ip.address();
+}
+
 const extensions = [ 'png', 'jpg', '.jpeg' ];
 
 var server = {
@@ -31,7 +60,7 @@ var server = {
   },
   views: [ ],
   currentView: -1,
-  ip: ip.address(),
+  ip: ipaddress,
   hostname: os.hostname(),
   path: ""
 } 
